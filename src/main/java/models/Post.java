a2ca2300a234f573bfcf75cc438b088ddaf110a5
Abel <abelberhane011@gmail.com>
2019-11-14 14:56:01 +0300

    [finishes #16975064] tests getter methods

--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -1,4 +1,34 @@
 package models;
 
+
+import java.time.LocalDateTime;
+
 public class Post {
+
+    private int id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+
+    public int getId() {
+        return this.id = 0;
+    }
+
+    public String getTitle() {
+        return this.title = "Motivation";
+    }
+
+    public String  getContent() {
+        return this.content = "This is a content part";
+    }
+
+    public LocalDateTime getCreatedAt(){
+        return this.createdAt = LocalDateTime.now();
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/test/java/models/PostTest.java
+++ src/test/java/models/PostTest.java
@@ -1,4 +1,53 @@
 package models;
 
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class PostTest {
+
+    @Test
+    public void PostObjectInstantiatesCorrectly_true(){
+        Post post = new Post();
+        assertTrue(true);
+    }
+
+    @Test
+    public void GetsGetterMethods_getsId(){
+        Post post = new Post();
+        int expected = 1;
+        assertEquals(expected, post.getId() + 1);
+    }
+    @Test
+    public void GetsGetterMethods_getsTitle(){
+        Post post = new Post();
+        String expected = "Motivation";
+        assertEquals(expected, post.getTitle());
+    }
+    @Test
+    public void GetsGetterMethods_getsContent(){
+        Post post = new Post();
+        String expected = "This is a content part";
+        assertEquals(expected, post.getContent());
+    }
+    @Test
+    public void GetsGetterMethods_getsDate(){
+        Post post = new Post();
+        LocalDate expected = LocalDateTime.now().toLocalDate();
+        assertEquals(expected, post.getCreatedAt().toLocalDate());
+    }
+//    @Test
+//    public void SetsSetterMethods_setsId(){
+//        Post post = new Post();
+//        int expected = 2;
+//        assertEquals(1, post.setId());
+//    }
+
+
+
 }
